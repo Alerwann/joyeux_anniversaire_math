1a9a9ca3ebac32a192f9b6f47b12896be9f57476
Alerwann <alerwann411@gmail.com>
2026-06-08 09:31:36 +0200

    creation du site d'anniversaire de math

---
+++ src/app/joyeuxAnniv/page.js
@@ -0,0 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect } from "react";
+
+export default function MesageAnniv() {
+  return (
+    <div className="h-full">
+      <header className="relative w-full aspect-1500/92">
+        <Image
+          src="/header.png"
+          alt="acc"
+          fill
+          sizes="100vw"
+          className="object-cover"
+          priority
+        />
+      </header>
+      <main className="flex flex-col md:flex-row w-full h-100vh items-center justify-around gap-5 ">
+        <div>
+          <div className="w-full max-w-[200px]">
+            <video
+              src={
+                "https://media.tenor.com/fjFefVXbjosAAAPo/funny-dog-dance-dancing-dog.mp4"
+              }
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-full rounded-lg"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <h1 className="text-center text-5xl font-extrabold italic">
+            Joyeux Anniversaires Math !
+          </h1>
+          <p className="text-center text-2xl">
+            Je ne sais pas si tu as déjà eu un joyeux anniversaire comme cela !
+          </p>
+          <p className="text-center text-2xl">
+            C'est pour rendre le passage de la quarantaine plus attrayant
+          </p>
+        </div>
+        <div className="w-full max-w-[200px]">
+          <video
+            src={"https://media.tenor.com/-kx4msGXmN8AAAPo/happy-dance.mp4"}
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="w-full rounded-lg"
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,64 +2,368 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div>
+      <header className="relative w-full aspect-1500/92 ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src={"/header.png"}
+          alt="acc"
+          fill
+          sizes="100vw"
+          className="object-cover"
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+      </header>
+      <h2 className="text-center text-5xl font-extrabold">
+        Anniversaires du 8 Juin
+      </h2>
+      <div className="m-15 grid grid-cols-2 md:grid-cols-5 gap-2 ">
+        <div className="tile__item">
+          <a className="tile" href="./joyeuxAnniv">
+            <div className="">
+              <img
+                src="/math.jpeg"
+                srcSet="/math-190.png 190w, /math-250.png 250w, /math-300.png 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Math Meyer"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Mathieu Meyer</p>
+
+            <p className="tile__description type-14-16">
+              Entrepreneur et Ami de longue date
+            </p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>40</span>
+            </p>
           </a>
         </div>
-      </main>
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/charlotte-lawrence.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/lawrence-charlotte-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/lawrence-charlotte-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/lawrence-charlotte-large.jpg 250w, https://www.famousbirthdays.com/faces/lawrence-charlotte-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Charlotte Lawrence"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Charlotte Lawrence, 25</p>
+
+            <p className="tile__description type-14-16">Chanteur Pop</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>21</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/francesca-capaldi.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/capaldi-francesca-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/capaldi-francesca-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/capaldi-francesca-large.jpg 250w, https://www.famousbirthdays.com/faces/capaldi-francesca-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Francesca Capaldi"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Francesca Capaldi, 21</p>
+
+            <p className="tile__description type-14-16">
+              Comédienne de Télévision
+            </p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>22</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/robert-lopez-youtubestar.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/lopez-robert-youtubestar-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/lopez-robert-youtubestar-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/lopez-robert-youtubestar-large.jpg 250w, https://www.famousbirthdays.com/faces/lopez-robert-youtubestar-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Rob Lopez"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Rob Lopez, 29</p>
+
+            <p className="tile__description type-14-16">Star YouTube</p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/hayes-grier.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/grier-hayes-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/grier-hayes-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/grier-hayes-large.jpg 250w, https://www.famousbirthdays.com/faces/grier-hayes-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Hayes Grier"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Hayes Grier, 25</p>
+
+            <p className="tile__description type-14-16">Star d'Instagram</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>24</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/clayton-bush.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/bush-clayton-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/bush-clayton-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/bush-clayton-large.jpg 250w, https://www.famousbirthdays.com/faces/bush-clayton-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Clayton Bush"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Clayton Bush, 25</p>
+
+            <p className="tile__description type-14-16">Star d'Instagram</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>25</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/anthony-beauvillier.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/beauvillier-anthony-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/beauvillier-anthony-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/beauvillier-anthony-large.jpg 250w, https://www.famousbirthdays.com/faces/beauvillier-anthony-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Anthony Beauvillier"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Anthony Beauvillier, 28</p>
+
+            <p className="tile__description type-14-16">Joueur de Hockey</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>26</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/luana-kazaki.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/faces/large-default.jpg"
+                srcSet="https://www.famousbirthdays.com/faces/large-default.jpg 190w, https://www.famousbirthdays.com/faces/large-default.jpg 250w, https://www.famousbirthdays.com/faces/large-default.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Luana Kazaki"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Luana Kazaki, 29</p>
+
+            <p className="tile__description type-14-16">Star TikTok</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>27</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/eden-zaken.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/zaken-eden-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/zaken-eden-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/zaken-eden-large.jpg 250w, https://www.famousbirthdays.com/faces/zaken-eden-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Eden Ben Zaken"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Eden Ben Zaken, 31</p>
+
+            <p className="tile__description type-14-16">Chanteur Pop</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>28</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/luan-alencar.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/alencar-luan-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/alencar-luan-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/alencar-luan-large.jpg 250w, https://www.famousbirthdays.com/faces/alencar-luan-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Luan Alencar"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Luan Alencar, 20</p>
+
+            <p className="tile__description type-14-16">Star d'Instagram</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>29</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/coralie-balmy.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/balmy-coralie-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/balmy-coralie-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/balmy-coralie-large.jpg 250w, https://www.famousbirthdays.com/faces/balmy-coralie-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Coralie Balmy"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Coralie Balmy, 38</p>
+
+            <p className="tile__description type-14-16">Nageur(se)</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>30</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/zoe-land2.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/thumbnails/zoe-land2-medium.jpg"
+                srcSet="https://www.famousbirthdays.com/thumbnails/zoe-land2-medium.jpg 190w, https://www.famousbirthdays.com/thumbnails/zoe-land2-large.jpg 250w, https://www.famousbirthdays.com/faces/zoe-land2-image.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="zoe.land2"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> zoe.land2, 23</p>
+
+            <p className="tile__description type-14-16">Star TikTok</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>31</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="tile__item">
+          <a
+            className="tile"
+            href="https://fr.famousbirthdays.com/people/niko-mcbride.html"
+          >
+            <div className="tile__picture">
+              <img
+                src="https://www.famousbirthdays.com/faces/large-default.jpg"
+                srcSet="https://www.famousbirthdays.com/faces/large-default.jpg 190w, https://www.famousbirthdays.com/faces/large-default.jpg 250w, https://www.famousbirthdays.com/faces/large-default.jpg 300w"
+                sizes="(max-width: 767px) 50vw, (max-width: 1023px) 20vw, 190px"
+                width="190"
+                height="190"
+                alt="Niko Bear McBride"
+                loading="lazy"
+                decoding="async"
+              />
+            </div>
+            <p className="type-16-18-small"> Niko Bear McBride, 7</p>
+
+            <p className="tile__description type-14-16">Star YouTube</p>
+
+            <p className="tile__star-ranking type-16-1">
+              <span>32</span>
+            </p>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
